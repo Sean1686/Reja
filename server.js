2b9d6133efc8970c3873c51ad3b149db0fe65ee3
Sean1686 <khudayberdi11@gmail.com>
2025-12-27 19:49:25 +0900

    feat: develop author  router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web server is running...");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("user.json", "utf-8", (err, data) => {
+    if (!err) {
+        user = JSON.parse(data);
+    } else {
+        console.log("Error reading user.json:", err);
+    }
+});
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -10,12 +20,16 @@ app.use(express.urlencoded({ extended: true }));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render("harid")
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get('/', function (req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);
-let PORT = 30000;
+let PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
