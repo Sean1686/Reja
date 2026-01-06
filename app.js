eb25a4fa6d364f0e29295f4ed78a5fabd36a5017
Sean1686 <khudayberdi11@gmail.com>
2026-01-06 17:57:22 +0900

    Reja loyihamizni yanukiy bosqich logic

--- app.js
+++ app.js
@@ -47,6 +47,26 @@ function(err, response) {
 })
 });
 
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data)
+  db.collection("plans").findOneAndUpdate(
+  {_id: new mongodb.ObjectId(data.id)},
+  {$set: {reja: data.new_input}},
+  function (err, response) {
+    res.json({state: "success"})
+  }
+  );
+});
+
+app.post("/delete-all", (req, res) => {
+  if(req.body.delete_all) [
+    db.collection("plans").deleteMany(function() {
+      res.json({state: "All plans have deleted"})
+    })
+  ]
+});
+
 app.get("/", (req, res) => {
   db.collection("plans")
     .find()
--- public/browser.js
+++ public/browser.js
@@ -1,15 +1,18 @@
-// const { response } = require("../app");
 
 console.log("Frontend JS is working");
 
 function itemTemplate(item) {
-  return `<li class="list-group-item list-group-item-info flex items-center justify-between">
-    <span class="item-text">${item.reja}</span>
-    <div>
-      <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-      <button class="delete-me btn btn-danger btn-sm">Delete</button>
-    </div>
-  </li>`;
+  return   `<li class="list-group-item d-flex align-items-center justify-content-between">
+            <span class="item-text">${item.reja}</span>
+            <div class="btn-group">
+            <button
+                data-id="${item._id}"
+            class="edit-me btn btn-secondary btn-sm">Edit</button>
+            <button
+                data-id="${item._id}"
+            class="delete-me btn btn-danger btn-sm">Delete</button>
+            </div>
+          </li>`;
 }
 
 let createInput = document.getElementById("create-input");
@@ -45,13 +48,37 @@ document.addEventListener("click", function(e) {
     });
     }
   }
-  })
-//   } else if(e.target.classList.contains("edit-me")) {
-//     alert("Are you sure? you want to edit it!");
-//     if(confirm("Do you want to edit it?")) {
-//       alert("You pressed the agreement button!");
-//     } else {
-//       alert("You pressed the disagreement button!");
-//     }
-//   }
-// });
+  
+  if (e.target.classList.contains("edit-me")) {
+  let userInput = prompt("Make changes",
+   e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+  );
+  if (userInput) {
+    axios
+      .post("/edit-item", {
+        id: e.target.getAttribute("data-id"),
+        new_input: userInput
+      })
+      .then((response) => {
+        console.log(response)
+        e.target.parentElement.parentElement.querySelector(
+        ".item-text")
+        .innerHTML = userInput;
+      })
+      .catch(() => {
+        console.log("Please, try again!");
+      });
+  }
+  }
+});
+
+document.getElementById("clean-all").addEventListener("click",
+   function () {
+   axios.post("/delete-all", {delete_all: true}).then((response) => {
+    alert(response.data.state);
+    document.location.reload();
+   })
+   .catch((err) => {
+    console.log("Please, try again!")
+   })
+});
