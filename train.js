c3d7ce7bd595b36fe78f2dcf2be8271a22959df4
Sean1686 <khudayberdi11@gmail.com>
2026-01-06 20:44:32 +0900

    Reja loyihamizni yakuniy bosqich logic

--- train.js
+++ train.js
@@ -1,17 +1,67 @@
+// MITTask D
+
+class Shop {
+  constructor(pepsi, fanta, cola) {
+    this.pepsi = pepsi;
+    this.fanta = fanta;
+    this.cola = cola;
+  }
+
+  sellingTime() {
+    const now = new Date();
+    return now.getHours() + ":" + now.getMinutes();
+  }
+
+  shopQoldiq() {
+    console.log(
+      `Hozir ${this.sellingTime()} da ${this.pepsi} ta pepsi, ${this.fanta} ta fanta va ${this.cola} ta cola mahsulotlari mavjud!`
+    );
+  }
+
+  selling(product, count) {
+    this[product] -= count;
+    console.log(
+      `${this.sellingTime()} da ${count} ta ${product} sotildi!`
+    );
+  }
+
+  buying(product, count) {
+    this[product] += count;
+    console.log(
+      `${this.sellingTime()} da ${count} ta ${product} sotib olindi!`
+    );
+  }
+}
+
+
+
+const statusof = new Shop(7, 3, 6);
+
+statusof.shopQoldiq();
+
+statusof.selling("pepsi", 2);
+statusof.buying("cola", 4);
+
+statusof.shopQoldiq();
+
+
+
+
+
+
 // MIT task C
 
-function checkContent (firststr, secondstr) {
-    if(firststr.length !== secondstr.length) return false;
+// function checkContent (firststr, secondstr) {
+//     if(firststr.length !== secondstr.length) return false;
 
-    const sorted1 = firststr.split('').sort().join('');
-    const sorted2 = secondstr.split('').sort().join('');
+//     const sorted1 = firststr.split('').sort().join('');
+//     const sorted2 = secondstr.split('').sort().join('');
 
-   return sorted1 === sorted2;
-}
-console.log(checkContent("mitgroup", "timpourg"));
-console.log(checkContent("true", "eurt"));
-console.log(checkContent("hello", "world"));
-
+//    return sorted1 === sorted2;
+// }
+// console.log(checkContent("mitgroup", "timpourg"));
+// console.log(checkContent("true", "eurt"));
+// console.log(checkContent("hello", "world"));
 
 
 
